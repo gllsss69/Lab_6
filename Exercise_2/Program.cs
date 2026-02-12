0225c2ca63602ccadd151f8af4c49e0a5f459dae
gllsss69 <maksympetyk08@gmail.com>
2026-02-12 22:59:59 +0200

    Завдання 2.  Космічні об'єкти; Проектування

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -1,5 +1,44 @@
 ﻿namespace Exercise_2
 {
+    
+    
+        public abstract class SpaceBody
+        {
+            public string Name { get; set; }
+            public double DistanceFromSun { get; set; }
+
+            public SpaceBody(string name, double distance)
+            {
+                Name = name;
+                DistanceFromSun = distance;
+            }
+
+           
+            public abstract void CalculateOrbitParameters();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     internal class Program
     {
         static void Main(string[] args)
@@ -7,4 +46,4 @@
             Console.WriteLine("Hello, World!");
         }
     }
-}
+
