3a6fae45da34ff5efc3515e12aaebad23ccd21eb
gllsss69 <maksympetyk08@gmail.com>
2026-02-12 23:16:37 +0200

    2. Наслідування (Planet, Asteroid, Comet)

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -1,39 +1,67 @@
-﻿namespace Exercise_2
+﻿using System.Text;
+
+namespace Exercise_2
 {
-    
-    
-        public abstract class SpaceBody
+
+
+    public abstract class SpaceBody
+    {
+        public string Name { get; set; }
+        public double DistanceFromSun { get; set; }
+
+        public SpaceBody(string name, double distance)
         {
-            public string Name { get; set; }
-            public double DistanceFromSun { get; set; }
-
-            public SpaceBody(string name, double distance)
-            {
-                Name = name;
-                DistanceFromSun = distance;
-            }
-
-           
-            public abstract void CalculateOrbitParameters();
+            Name = name;
+            DistanceFromSun = distance;
         }
+
+
+        public abstract void CalculateOrbitParameters();
     }
 
 
 
 
+    public class Planet : SpaceBody
+    {
+        public Planet(string name, double distance) : base(name, distance) { }
+
+        public override void CalculateOrbitParameters()
+        {
+            // Спрощена формула Кеплера: T = sqrt(a^3)
+            double period = Math.Sqrt(Math.Pow(DistanceFromSun, 3));
+            Console.WriteLine($"\nПланета {Name}: Стабільна еліптична орбіта. " +
+                            $"Рік триває приблизно {period:F2} земних років.");
+        }
+    }
 
 
+    public class Asteroid : SpaceBody
+    {
+        public Asteroid(string name, double distance) : base(name, distance) { }
+
+        public override void CalculateOrbitParameters()
+        {
+            Console.WriteLine($"\nАстероїд {Name}: Рухається у складі астероїдного поясу на відстані {DistanceFromSun} AU.");
+        }
+    }
 
 
+    public class Comet : SpaceBody
+    {
+        public double Eccentricity { get; set; }
 
+        public Comet(string name, double distance, double eccentricity) : base(name, distance)
+        {
+            Eccentricity = eccentricity;
+        }
 
-
-
-
-
-
-
-
+        public override void CalculateOrbitParameters()
+        {
+            Console.WriteLine($"\nКомета {Name}: Дуже витягнута орбіта (ексцентриситет: {Eccentricity}). " +
+                            $"Наближається до Сонця на {DistanceFromSun} AU.");
+        }
+    }
 
 
 
@@ -43,7 +71,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
         }
     }
-
+}
