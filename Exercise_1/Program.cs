eda94fafae7b531b26323f988cd48d154be0dace
gllsss69 <maksympetyk08@gmail.com>
2026-02-12 22:13:00 +0200

    Завданя 1. Проектування

--- Exercise_1/Program.cs
+++ Exercise_1/Program.cs
@@ -1,5 +1,30 @@
 ﻿namespace Exercise_1
 {
+    public abstract class Vehicle
+    {
+        
+        public string Brand { get; set; }
+        public double Speed { get; set; }
+
+       
+        public Vehicle(string brand, double speed)
+        {
+            Brand = brand;
+            Speed = speed;
+        }
+
+        
+        public abstract void Move();
+    }
+
+
+
+
+
+
+
+
+
     internal class Program
     {
         static void Main(string[] args)
